fc33f008e0eba6dcd8c2cceb0eea1c7996a4d079
Alex Athanasiou <aatanasziu@gmail.com>
2018-05-29 15:18:22 +0200

    new chore: Person parent class, Student - Mentor - Sponsor child class, two overriding methods Introduce and GetGoal, child class with Cohort

---
+++ week-04/day-02/green-fox/green-fox/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace green_fox
+{
+    class Student : Person
+    {
+        private string previousOrganization;
+        private int skippedDays;
+
+        public Student(string name, int age, string gender, string previousOrganization)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.previousOrganization = previousOrganization;
+            skippedDays = 0;
+        }
+        public Student()
+        {
+            name = "Jane Doe";
+            age = 30;
+            gender = "female";
+            previousOrganization = "The School of Life";
+            skippedDays = 0;
+        }
+
+        public override void GetGoal()
+        {
+            Console.WriteLine("Be a junior software developer.");
+        }
+
+        public override void Introduce()
+        {
+            Console.WriteLine("Hi, I'm {0}, a {1} year old {2} from {3} who skipped {4} days from the course already.", name, age, gender, previousOrganization, skippedDays);
+        }
+
+        public int SkipDays(int numberOfDays)
+        {
+            return skippedDays += numberOfDays;
+        }
+
+    }
+}
