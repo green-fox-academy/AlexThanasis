63431cbb81d413b7b80a7565d50f191800db3842
Alex Athanasiou <aatanasziu@gmail.com>
2018-06-22 16:29:28 +0200

    refactoring to use SQLite

---
+++ week-06/day-04/TODO/TODO/Database.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Text;
+
+namespace TODO
+{
+    class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source=database.sqlite3");
+            if (!File.Exists(@"C:\Users\Alex_GR\Desktop\Green Fox Academy\greenfox\AlexThanasis\week-06\day-04\TODO\TODO\bin\Debug\netcoreapp2.0\database.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("database.sqlite3");
+                Console.WriteLine("Database created");
+            }
+        }
+
+        public void OpenConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+        }
+
+
+        public static void Adding(string input, Database dbinput)
+        {
+            string query = "INSERT INTO todos (`text`) VALUES (@text)";
+            SQLiteCommand myCommand = new SQLiteCommand(query, dbinput.myConnection);
+            dbinput.OpenConnection();
+            myCommand.Parameters.AddWithValue("@text", input);
+            DateTime dateNow = DateTime.Now;
+            // myCommand.Parameters.AddWithValue("@createdAt", "");
+            myCommand.ExecuteNonQuery();
+            dbinput.CloseConnection();
+            //Console.WriteLine("New tasks has been added: {0}", result);
+        }
+
+        public static void Removing(int num, Database dbinput)
+        {
+            string query = "DELETE FROM todos WHERE (@id)";
+            SQLiteCommand myCommand = new SQLiteCommand(query, dbinput.myConnection);
+            dbinput.OpenConnection();
+            myCommand.Parameters.AddWithValue("@id", "id = " + num);
+            myCommand.ExecuteNonQuery();
+            dbinput.CloseConnection();
+        }
+
+        public static void Loading(Database dbinput)
+        {
+            string query = "SELECT * FROM todos";
+            SQLiteCommand myCommand = new SQLiteCommand(query, dbinput.myConnection);
+            dbinput.OpenConnection();
+            SQLiteDataReader result = myCommand.ExecuteReader();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Console.WriteLine("ID: {0} - Content: {1} - Created at: {2}, Completed at: {3}", result["id"], result["text"], result["createdAt"], result["completedAt"]);
+                }
+            }
+            dbinput.CloseConnection();
+        }
+    }
+}
---
+++ week-06/day-04/TODO/TODO/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+
+namespace TODO
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            PrintIntro();
+            Database databaseObject = new Database();
+            string argsFromConsoleReadline = Console.ReadLine();
+            Database.Adding("kvét venni", databaseObject);
+            string[] commandInputs = argsFromConsoleReadline.Split(",");
+            string[] commands = { "-a", "-m", "-l", "-r", "-c", "-e" };
+            for (int i = 0; i < commandInputs.Length; i++)
+            {
+                if (commandInputs[i].Contains("-a"))
+                {
+                    Database.Adding(commandInputs[i + 1], databaseObject);
+                }
+                if (commandInputs[i].Contains("-r"))
+                {
+                    Database.Removing(Int32.Parse(commandInputs[i + 1].Trim()), databaseObject);
+                }
+                if (commandInputs[i].Contains("-c"))
+                {
+                    Database.Removing(Int32.Parse(commandInputs[i + 1].Trim()), databaseObject);
+                }
+                if (commands.Any(element => element.Equals(commandInputs[i])))
+                {
+                    CommandFinder(commandInputs[i], databaseObject);
+                }
+            }
+            Console.ReadLine();
+        }
+        
+        public static void PrintIntro()
+        {
+            string path = @"../../../files/printinfo.txt";
+            string printIntroContent = File.ReadAllText(path);
+            Console.WriteLine(printIntroContent);
+        }
+
+        public static void CommandFinder(string input, Database inputdb)
+        {
+            if (input.Equals("-l"))
+            {
+                Database.Loading(inputdb);
+            }
+        }
+    }
+}
