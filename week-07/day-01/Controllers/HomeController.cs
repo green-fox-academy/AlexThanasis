8706c0a99203f9cd5ad18852300b2d57439a61cb
Alex Athanasiou <aatanasziu@gmail.com>
2018-06-25 12:48:48 +0200

    new chore and new method for Hello World

---
+++ week-07/day-01/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("hellóka");
+        }
+
+        [Route("hello")]
+        public IActionResult Hello(string name)
+        {
+            return Content("hello miszter " + name + "!");
+        }
+
+    }
+}
