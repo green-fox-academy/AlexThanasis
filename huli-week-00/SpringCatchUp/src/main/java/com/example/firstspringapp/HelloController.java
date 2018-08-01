180a046b56267c2196d34e6e1c3caa30bc5db427
Alex Athanasiou <aatanasziu@gmail.com>
2018-08-01 17:02:36 +0200

    new route added

--- huli-week-00/SpringCatchUp/src/main/java/com/example/firstspringapp/HelloController.java
+++ huli-week-00/SpringCatchUp/src/main/java/com/example/firstspringapp/HelloController.java
@@ -1,8 +1,18 @@
 package com.example.firstspringapp;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello Whatever";
+    }
+
+    @RequestMapping("/hello")
+    public String sayHi(){
+        return "Hello Hi";
+    }
 }
