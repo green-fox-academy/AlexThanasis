51e4fa22c9817a22ac01fe4f7237bebee74bd678
Alex Athanasiou <aatanasziu@gmail.com>
2018-08-02 16:24:13 +0200

    new feat: index html: dynamic table rows

--- huli-week-00/TheRedditCloneJavaSpringBoot/src/main/java/com/alexsystems/theredditclonejavaspringboot/Models/Post.java
+++ huli-week-00/TheRedditCloneJavaSpringBoot/src/main/java/com/alexsystems/theredditclonejavaspringboot/Models/Post.java
@@ -17,6 +17,14 @@ public class Post {
         this.score = score;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public Post() {
     }
 
