abe13781284e727c683036a974dc45fc8a11e637
Alex Athanasiou <aatanasziu@gmail.com>
2018-08-02 15:39:19 +0200

    fix: model all getters and setters

--- huli-week-00/TheRedditCloneJavaSpringBoot/src/main/java/com/alexsystems/theredditclonejavaspringboot/Models/Post.java
+++ huli-week-00/TheRedditCloneJavaSpringBoot/src/main/java/com/alexsystems/theredditclonejavaspringboot/Models/Post.java
@@ -10,4 +10,53 @@ public class Post {
     private int score;
     private long userId;
 
+    public Post(String content, String url, Date createdAt, int score, long userId) {
+        this.content = content;
+        this.url = url;
+        this.createdAt = createdAt;
+        this.score = score;
+    }
+
+    public Post() {
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
 }
