62947921d4ed710dfdaf7d5f6b86c6c30bfc79b7
Alex Athanasiou <aatanasziu@gmail.com>
2018-08-02 15:37:11 +0200

    new feat: add model for posts

--- huli-week-00/TheRedditCloneJavaSpringBoot/src/main/java/com/alexsystems/theredditclonejavaspringboot/Models/Post.java
+++ huli-week-00/TheRedditCloneJavaSpringBoot/src/main/java/com/alexsystems/theredditclonejavaspringboot/Models/Post.java
@@ -1,4 +1,13 @@
 package com.alexsystems.theredditclonejavaspringboot.Models;
 
+import java.util.Date;
+
 public class Post {
+    private long id;
+    private String content;
+    private String url;
+    private Date createdAt;
+    private int score;
+    private long userId;
+
 }
