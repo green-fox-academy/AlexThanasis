3d096ef3c928cb769204e62cca44575a72bd6b63
Alex Athanasiou <aatanasziu@gmail.com>
2018-08-02 10:46:58 +0200

    new chore: Database practice

---
+++ huli-week-00/databasepractice/src/main/java/com/data/databasepractice/DatabasepracticeApplication.java
@@ -0,0 +1,12 @@
+package com.data.databasepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasepracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabasepracticeApplication.class, args);
+    }
+}
