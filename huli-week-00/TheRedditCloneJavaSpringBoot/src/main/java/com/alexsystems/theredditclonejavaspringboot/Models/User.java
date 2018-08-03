3449c0fc08a45b2bdfb0c107468f3216fd3c68b7
Alex Athanasiou <aatanasziu@gmail.com>
2018-08-03 16:21:30 +0200

    new field for Model User: email, in the order of storing e-mail addresses

--- huli-week-00/TheRedditCloneJavaSpringBoot/src/main/java/com/alexsystems/theredditclonejavaspringboot/Models/User.java
+++ huli-week-00/TheRedditCloneJavaSpringBoot/src/main/java/com/alexsystems/theredditclonejavaspringboot/Models/User.java
@@ -14,11 +14,13 @@ public class User {
     private String name;
     private String description;
     private String password;
+    private String email;
 
-    public User(String name, String description, String password) {
+    public User(String name, String description, String password, String email) {
         this.name = name;
         this.description = description;
         this.password = password;
+        this.email = email;
     }
 
     public User() {
@@ -54,5 +56,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
