36121375d8802c0e59513d387c2ae2bda28050b0
Alex Athanasiou <aatanasziu@gmail.com>
2018-08-06 17:34:46 +0200

    Java Spring Boot Security practise with new chore and new methods added: RestController, Service layer and a special SecurityConfig class

---
+++ huli-week-00/demo/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.security;
+
+import com.example.demo.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/sign_up").permitAll()
+                .antMatchers("*/floor1/**").hasRole("USER")
+                .antMatchers("*/floor2/**").hasRole("ADMIN");
+    }
+}
---
+++ huli-week-00/demo/src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("batman", "pass", AuthorityUtils.createAuthorityList("ROLE_USER"));
+    }
+}
