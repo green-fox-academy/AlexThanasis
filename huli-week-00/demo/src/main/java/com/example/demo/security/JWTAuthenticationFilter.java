5513fa9997bad55956f8a7657b9936278f3c63ee
Alex Athanasiou <aatanasziu@gmail.com>
2018-08-07 16:54:18 +0200

    finishing Spring Boot Security tutorial, final steps

--- huli-week-00/demo/src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
+++ huli-week-00/demo/src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.example.demo.security;
 import com.example.demo.model.ApplicationUser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,8 +19,9 @@ import java.io.IOException;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
-import static com.example.demo.security.SecurityConstants.EXPIRATION_TIME;
+import static com.example.demo.security.SecurityConstants.*;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private AuthenticationManager authenticationManager;
@@ -39,8 +41,15 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain, Authentication authResult) throws IOException, ServletException {
         ZonedDateTime expirationTimeUTC = ZonedDateTime.now(ZoneOffset.UTC).plus(EXPIRATION_TIME, ChronoUnit.MILLIS);
-        String token = Jwts.builder().setSubjects(((User)authResult.getPrincipal().getUsername())
+        String token = Jwts.builder().setSubject(((User)authResult.getPrincipal()).getUsername())
+                .setExpiration(Date.from(expirationTimeUTC.toInstant()))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+        response.getWriter().write(token);
+        response.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
     }
 }
