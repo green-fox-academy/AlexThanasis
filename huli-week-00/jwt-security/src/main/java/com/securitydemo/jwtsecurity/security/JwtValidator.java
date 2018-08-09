83de4b27abb8d270b5975ca11893fd59e3c5dc00
Alex Athanasiou <aatanasziu@gmail.com>
2018-08-09 10:57:21 +0200

    refreshing the repo

--- huli-week-00/jwt-security/src/main/java/com/securitydemo/jwtsecurity/security/JwtValidator.java
+++ huli-week-00/jwt-security/src/main/java/com/securitydemo/jwtsecurity/security/JwtValidator.java
@@ -1,4 +1,16 @@
 package com.securitydemo.jwtsecurity.security;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtValidator {
+
+    private String secret;
+
+    public Object validate(String token) {
+
+        Claims claims = Jwts.parser()
+                .setSingingKey(secret);
+        return null;
+    }
 }
