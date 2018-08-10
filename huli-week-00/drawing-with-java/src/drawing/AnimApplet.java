9c1462ba15b04c38165c16702617b97cf252d874
Alex Athanasiou <aatanasziu@gmail.com>
2018-08-10 22:19:53 +0200

    new drawing exercises

---
+++ huli-week-00/drawing-with-java/src/drawing/AnimApplet.java
@@ -0,0 +1,41 @@
+package drawing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class AnimApplet extends JApplet {
+
+    boolean animationDone;
+    int centerX, centerY;
+    int w, h;
+    public void init(){
+        w = h = 100;
+        new Thread(){
+            @Override
+            public void run(){
+                while(!animationDone){
+                    updateAnimation();
+                    repaint();
+                    delayAnimation();
+                }
+            }
+        }.start();
+    }
+    public void updateAnimation(){
+        w +=2;
+        h +=2;
+    }
+    public void delayAnimation(){
+       try{
+           Thread.sleep(30);
+       }
+       catch (InterruptedException e){
+           e.printStackTrace();
+       }
+    }
+    public void paint(Graphics g){
+        g.setColor(Color.GREEN);
+        g.fillOval(centerX - w/2, centerY - h/2, 100, 100);
+    }
+}
---
+++ huli-week-00/drawing-with-java/src/drawing/drawAppStart.java
@@ -0,0 +1,13 @@
+package drawing;
+import javax.swing.*;
+
+public class drawAppStart {
+    public static void main(String[] args){
+        JFrame f = new JFrame("Drawing");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        AnimApplet drawApp = new AnimApplet();
+        f.add(drawApp);
+        f.setSize(400, 250);
+        f.setVisible(true);
+    }
+}
