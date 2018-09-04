f3a187667cf8fd5bd2e4f21c207f2696158642be
Alex Athanasiou <aatanasziu@gmail.com>
2018-09-04 22:34:07 +0200

    new feat: message service for Angular Tour-de-Heroes project

--- side projects/JavaScript/Angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ side projects/JavaScript/Angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,17 +8,23 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  
+   
+  selectedHero: Hero;
+ 
   heroes : Hero[];
  
-  selectedHero: Hero;
-
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-}
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
