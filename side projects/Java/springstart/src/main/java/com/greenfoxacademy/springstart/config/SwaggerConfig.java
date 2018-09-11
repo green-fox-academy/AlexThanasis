606a5e615a51a6211cd6a63cde818dd92aa380c6
Alex Athanasiou <aatanasziu@gmail.com>
2018-09-11 16:50:24 +0200

    new feat: Swagger configuation in SwaggerConfig class

--- side projects/Java/springstart/src/main/java/com/greenfoxacademy/springstart/config/SwaggerConfig.java
+++ side projects/Java/springstart/src/main/java/com/greenfoxacademy/springstart/config/SwaggerConfig.java
@@ -1,5 +1,18 @@
 package com.greenfoxacademy.springstart.config;
 
+import com.greenfoxacademy.springstart.controllers.HelloRESTController;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@PropertySource("classpath:swagger.properties")
+@ComponentScan(basePackageClasses = HelloRESTController.class)
+@Configuration
 public class SwaggerConfig {
-    
+
+    private static final String SWAGGER_API_VERSION = "1.0";
+    private static final String LICENSE_TEXT = "License";
+    private static final String title = "Products REST API";
 }
