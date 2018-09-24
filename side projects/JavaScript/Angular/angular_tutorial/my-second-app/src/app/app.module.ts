be540ec3d4bb34463bc694261750cc26635314a2
Alex Athanasiou <aatanasziu@gmail.com>
2018-09-24 16:04:20 +0200

    new feat: header component elements

--- side projects/JavaScript/Angular/angular_tutorial/my-second-app/src/app/app.module.ts
+++ side projects/JavaScript/Angular/angular_tutorial/my-second-app/src/app/app.module.ts
@@ -6,9 +6,6 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
-import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
@@ -20,14 +17,11 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipesListComponent,
-    RecipesDetailComponent,
-    RecipesItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     RecipeListComponent,
     RecipeDetailComponent,
-    RecipeItemComponent
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule
